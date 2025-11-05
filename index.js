8c2953a5a0a95998f2dd53877e594f7162875075
puya310 <puya310r@gmail.com>
2025-11-05 15:38:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 try {
   const name = core.getInput('name');
-  console.log(`Hello, ${name}! 👋`);
+  console.log(`Hello Hello, ${name}! 👋`);
 } catch (error) {
   core.setFailed(error.message);
 }
